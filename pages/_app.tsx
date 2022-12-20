b79ab32ed6733ee83b87c31e34aeb35b50b906cc
Ramadan <ramadan@Ramadans-MacBook-Pro.local>
2022-12-21 00:06:16 +0200

    add default font to Cairo in Tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,16 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from '../src/redux';
 import { appWithTranslation } from 'next-i18next';
+import Head from 'next/head';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <ThemeProvider theme={theme}>
+          <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900;1000" rel="stylesheet" />
+          </Head>
           <Component {...pageProps} />
         </ThemeProvider>
       </PersistGate>
