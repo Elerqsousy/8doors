3be7942e583c2d59f4f5d9ed55a8eb6235e17735
Dagi <dagi.zewdu.dz@gmail.com>
2022-12-20 18:42:58 +0300

    started integration of redux toolkit

---
+++ src/redux/reducers/accountReducer.ts
@@ -0,0 +1,51 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  accounts: [],
+  currentUser: null,
+  loading: true,
+  data: [],
+  error: false,
+  messageToken: null,
+};
+
+const accountSlice = createSlice({
+  name: 'account',
+  initialState,
+  reducers: {
+    addAccount(state, action) {
+      let Account = state.accounts.find((account) => account.uid === action.payload.id);
+      if (!Account) {
+        state.accounts.push(action.payload);
+      }
+    },
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload;
+    },
+    setMessageToken(state, action) {
+      state.messageToken = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getCurrentUser.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getCurrentUser.fulfilled, (state, action) => {
+      state.loading = false;
+      if (action.payload) {
+        state.currentUser = action.payload;
+      }
+      state.error = false;
+    });
+    builder.addCase(getCurrentUser.rejected, (state, action) => {
+      state.loading = false;
+      console.log('error...', action.payload);
+      state.error = action.payload;
+    });
+  },
+});
+const accountReducer = accountSlice.reducer;
+
+export const { addAccount, setCurrentUser, setMessageToken } = accountSlice.actions;
+
+export default accountReducer;
