2f5b15da08897f85745b4a73d96746514938b060
Dagi <dagi.zewdu.dz@gmail.com>
2022-12-21 10:19:22 +0300

    Some minor fixes on type

--- pages/_sites/clinic/signup.tsx
+++ pages/_sites/clinic/signup.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SignUpContainer from 'components/signup';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export const getServerSideProps = async ({ locale }) => ({
+export const getServerSideProps = async ({ locale }: any) => ({
   props: {
     ...(await serverSideTranslations(locale ?? 'en-US', ['common'])),
   },
