c7ee4db6eb4a8e3ddd07016d9aba2d4a689cbf9e
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-12-29 16:35:43 +0200

    Modify theme colors in tailwaind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        cairo: ["Cairo , sans-serif"]
+        cairo: ['Cairo , sans-serif'],
       },
+    },
+    colors: {
+      primary: '#01d8da;',
+      gray: {
+        lighter: '#f4f6f9',
+        light: '#9A9A9A',
+        mid: '#888',
+        dark: '#757575',
+        darker: '#455a64'
+      },
+      black: {
+        light: '#424242',
+        mid: '#2c2c2c',
+        dark: '#212529',
+      }
     },
   },
   plugins: [],
-}
+};
