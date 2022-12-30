72a926a9fafee79b87412af12e5a06587a17bf62
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-12-30 15:46:22 +0200

    edit i18next configuration primary language

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    i18n: {
-        locales: ['en-US', 'fr', 'ar'],
-        defaultLocale: 'fr',
-        localePath: require('path').resolve('./public/locales'),
-        reloadOnPrerender: process.env.NODE_ENV === 'development',
-    },
-}
+  i18n: {
+    locales: ['en-US', 'fr', 'ar'],
+    defaultLocale: 'en-US',
+    localePath: require('path').resolve('./public/locales'),
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
+  },
+};
