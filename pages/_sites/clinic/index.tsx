ba5ee84eb884bffb5062908e125c8c949584fb2e
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-12-30 20:02:54 +0200

    Update dashboard page

--- pages/_sites/clinic/index.tsx
+++ pages/_sites/clinic/index.tsx
@@ -6,12 +6,20 @@ export default function ClinicPage() {
     <Page title='Home Page' subTitle='sub-text' showSiderMenu>
       <Section title='Book Appointment' subtitle='Description text here...'>
         <div className='h-[100px] w-full bg-gray-lighter'>content 1</div>
+        <div className='flex flex-wrap gap-x-2.5 mt-5'>
+          <Button fit>submit</Button>
+          <Button fit disabled design='secondary'>cancel</Button>
+        </div>
       </Section>
       <Section className='w-1/2' title='Book Appointment'>
         <div className='h-[100px] w-full bg-gray-lighter'>content 2</div>
       </Section>
       <Section className='w-1/2'>
         <div className='h-[100px] w-full bg-gray-lighter'>content 3</div>
+        <div className='flex flex-wrap gap-x-2.5 mt-5'>
+          <Button>submit</Button>
+          <Button design='secondary'>cancel</Button>
+        </div>
       </Section>
       <Section className='w-1/3' title='Book Appointment'>
         <div className='h-[100px] w-full bg-gray-lighter'>content 4</div>
@@ -26,15 +34,11 @@ export default function ClinicPage() {
         <div className='h-[100px] w-full bg-gray-lighter'>content 4</div>
         <div className='flex flex-wrap gap-x-2.5 mt-5'>
           <Button>submit</Button>
-          <Button design='secondary'>submit</Button>
+          <Button design='secondary'>cancel</Button>
         </div>
       </Section>
       <Section className='w-1/3' title='Book Appointment'>
         <div className='h-[100px] w-full bg-gray-lighter'>content 5</div>
-        <div className='flex flex-wrap gap-x-2.5 mt-5'>
-          <Button fit>submit</Button>
-          <Button fit design='secondary'>submit</Button>
-        </div>
       </Section>
       <Section className='w-1/3' title='Book Appointment'>
         <div className='h-[100px] w-full bg-gray-lighter'>content 6</div>
