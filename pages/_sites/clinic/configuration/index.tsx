81d636aa8b41429e0658d0c6198b621448b26f21
Ramadan <ramadan@Ramadans-MacBook-Pro.local>
2023-01-02 19:42:58 +0200

    changes

--- pages/_sites/clinic/configuration/index.tsx
+++ pages/_sites/clinic/configuration/index.tsx
@@ -1,16 +1,16 @@
-import { Button, Page, Section } from 'components/shared';
+import { Button, CollapsibleSection, Page, Section } from 'components/shared';
 import React from "react";
 
 export default function Configuration() {
   return (
     <Page title='Configuration Page' subTitle='sub-text' showSiderMenu>
-      <Section title='Book Appointment' subtitle='Description text here...'>
+      <CollapsibleSection>
         <div className='h-[100px] w-full bg-gray-lighter'>content 1</div>
         <div className='flex flex-wrap gap-x-2.5 mt-5'>
           <Button fit>submit</Button>
           <Button fit disabled design='secondary'>cancel</Button>
         </div>
-      </Section>
+      </CollapsibleSection>
     </Page>
   );
 }
